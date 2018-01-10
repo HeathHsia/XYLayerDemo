0e476e6dc38ffbf76e0944bd029aa07784225c7c
HeathHsia <firehsia1204@gmail.com>
2018-01-10 11:07:09 +0800

    commit code

--- CoreAnimateLearning/CALayerDemo/CALayerDemo/CustomView.m
+++ CoreAnimateLearning/CALayerDemo/CALayerDemo/CustomView.m
@@ -23,10 +23,13 @@
 - (id<CAAction>)actionForLayer:(CALayer *)layer forKey:(NSString *)event
 {
     
+    
+    // 如果在block中修改计算变量的话, 需要用__block进行修饰
     // 在执行动画或者UI相关的操作的时候, 先进行当前线程的判断, 是否是主线程, 如果是主线程的话, 就直接进行动画操作, 如果不是的话, 异步调用主线程执行动画操作
+    
     if ([NSThread isMainThread]) {
-        
         // 当前的UI动画
+        
         
     }else {
         
